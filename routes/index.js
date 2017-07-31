709d99b5000ea60552aea1c6499206f98e2846de
Bernard Williams <bernard.l.williams@live.com>
2017-07-31 11:42:49 -0400

    Serve stylesheets statically with nginx.

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var path = require("path");
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.sendFile(path.join(__dirname, "index.html"));
+    //res.sendFile('public/index.html', {root: __dirname});
+    res.sendFile(path.resolve(__dirname, "../public/index.html"));
     //res.render('index', { title: 'Express' });
+
 });
 
 module.exports = router;
